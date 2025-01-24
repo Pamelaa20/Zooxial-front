cfacaa7290be231923a3e4d234d1de00ce81d024
Humberto Santos <1201hs@gmail.com>
2025-01-24 15:50:53 -0500

    create page veterinarias

--- zooxial/src/app/page.tsx
+++ zooxial/src/app/page.tsx
@@ -5,9 +5,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Página Principal</h1>
-      <Link href="/about" className="text-blue-500 underline mt-4">
-        Ir a la página Acerca de
+      <h1 className="text-4xl font-bold">Zooxial</h1>
+      <Link href="/veterinarias" className="text-blue-500 underline mt-4">
+        Veterinarias
       </Link>
     </div>
   );
--- zooxial/src/app/veterinarias/page.tsx
+++ zooxial/src/app/veterinarias/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function About() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Página Acerca de</h1>
+      <h1 className="text-4xl font-bold">Veterinarias</h1>
       <Link href="/" className="text-blue-500 underline mt-4">
         Volver a la Página Principal
       </Link>
