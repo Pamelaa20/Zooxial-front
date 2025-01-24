fb57dd487c6bb6ff9bc7bc4f7de45136c560af07
Humberto Santos <1201hs@gmail.com>
2025-01-24 18:37:43 -0500

    fix all pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div className="hero text-center py-12 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: "url('img/home.jpg')" }}>
-        <h1 className="text-3xl text-[#1abc9c]">
-          Zooxial conectando mascotas con el mejor cuidado.
+      <div className="hero text-center py-64 h-60 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: "url('img/home.jpg')" }}>
+        <h1 className="text-3xl text-white">
+          <span className="text-6xl text-[#FF709D]">Zooxial</span> conectando mascotas con el mejor cuidado.
         </h1>
-        <p className="text-gray-700">Tu mejor amigo.</p>
 
         <div className="form mt-4">
           <select className="p-2 m-2 border border-gray-300">
@@ -30,41 +29,47 @@ export default function Home() {
             <option>Accesorios</option>
             <option>Alertas</option>
           </select>
-          <button className="p-2 m-2 bg-[#1abc9c] text-white rounded hover:bg-[#16a085]">
+          <button className="p-2 m-2 bg-[#FF709D] text-white rounded hover:bg-[#16a085]">
             Buscar
           </button>
         </div>
       </div>
 
-      <section className="services flex flex-wrap justify-around py-8">
-        <div className="service w-60 text-center p-4 border border-gray-300 rounded m-2">
-          <h3 className="text-[#1abc9c]">Veterinarias</h3>
-          <p><b>Cuidamos de tu mascota como parte de tu familia.</b>
+      <section className="services flex justify-around py-8">
+        <div className="service w-1/3 text-justify p-4 border bg-white border-gray-300 rounded-md m-2">
+          <h3 className="text-[#FF709D] text-xl">Veterinarias</h3>
+          <b className="w-full">Cuidamos de tu mascota como parte de tu familia.</b>
+          <p>
           Nuestros veterinarios están comprometidos con la salud y el bienestar de tu mascota, brindando atención médica de calidad y amor incondicional.</p>
         </div>
-        <div className="service w-60 text-center p-4 border border-gray-300 rounded m-2">
-          <h3 className="text-[#1abc9c]">Veterinarios</h3>
-          <p><b>Profesionales dedicados a la salud animal.</b>
+        <div className="service w-1/3 text-justify p-4 border bg-white border-gray-300 rounded-md m-2">
+          <h3 className="text-[#FF709D] text-xl">Veterinarios</h3>
+          <b>Profesionales dedicados a la salud animal.</b>
+          <p>
           Nuestro equipo de veterinarios expertos está aquí para ofrecerte diagnósticos precisos y tratamientos efectivos, siempre con un enfoque personalizado para tu mascota.</p>
         </div>
-        <div className="service w-60 text-center p-4 border border-gray-300 rounded m-2">
-          <h3 className="text-[#1abc9c]">Alimentos</h3>
-          <p><b>Nutrición de calidad para cada etapa de tu mascota.</b>
+        <div className="service w-1/3 text-justify p-4 border bg-white border-gray-300 rounded-md m-2">
+          <h3 className="text-[#FF709D] text-xl">Alimentos</h3>
+          <b>Nutrición de calidad para cada etapa de tu mascota.</b>
+          <p>
           Te ofrecemos una variedad de alimentos especializados para satisfacer las necesidades nutricionales de tu mascota, desde cachorros hasta adultos mayores.</p>
         </div>
-        <div className="service w-60 text-center p-4 border border-gray-300 rounded m-2">
-          <h3 className="text-[#1abc9c]">Accesorios</h3>
-          <p><b>Lo mejor para tu mascota.</b>
+        <div className="service w-1/3 text-justify p-4 border bg-white border-gray-300 rounded-md m-2">
+          <h3 className="text-[#FF709D] text-xl">Accesorios</h3>
+          <b>Lo mejor para tu mascota.</b>
+          <p>
           Encuentra en nuestra tienda los accesorios más modernos y cómodos, desde camas y juguetes hasta collares y arneses, todo lo que tu mascota necesita para estar feliz y saludable.</p>
         </div>
-        <div className="service w-60 text-center p-4 border border-gray-300 rounded m-2">
-          <h3 className="text-[#1abc9c]">Alertas</h3>
-          <p><b>Mantén a tu mascota segura.</b>
+        <div className="service w-1/3 text-justify p-4 border bg-white border-gray-300 rounded-md m-2">
+          <h3 className="text-[#FF709D] text-xl">Alertas</h3>
+          <b>Mantén a tu mascota segura.</b>
+          <p>
           Recibe notificaciones y alertas sobre vacunación, citas de control y otros eventos importantes para el bienestar de tu mascota, todo al alcance de tu mano.</p>
         </div>
-        <div className="service w-60 text-center p-4 border border-gray-300 rounded m-2">
-          <h3 className="text-[#1abc9c]">Mis mascotas</h3>
-          <p><b>Comparte momentos especiales con tu mascota.</b>
+        <div className="service w-1/3 text-justify p-4 border bg-white border-gray-300 rounded-md m-2">
+          <h3 className="text-[#FF709D] text-xl">Mis mascotas</h3>
+          <b>Comparte momentos especiales con tu mascota.</b>
+          <p>
           Únete a nuestra comunidad y comparte fotos, historias y consejos sobre tus mascotas. Conéctate con otros amantes de los animales y forma parte de nuestra red social.</p>
         </div>
       </section>
---
+++ src/app/registrar/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Link from "next/link";
+
+export default function About() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Registrar</h1>
+      <Link href="/" className="text-blue-500 underline mt-4">
+        Volver a la Página Principal
+      </Link>
+    </div>
+  );
+}
