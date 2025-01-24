f81cd58abc7fd540fe042ed7335939600e6ce853
Humberto Santos <1201hs@gmail.com>
2025-01-24 15:45:54 -0500

    add nextjs

---
+++ zooxial/src/app/about/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Link from "next/link";
+
+export default function About() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Página Acerca de</h1>
+      <Link href="/" className="text-blue-500 underline mt-4">
+        Volver a la Página Principal
+      </Link>
+    </div>
+  );
+}
